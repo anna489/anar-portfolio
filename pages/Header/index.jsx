048424089d002acfae8463a758ed2d44bff5a3f9
anars mac <anarganbaatar@Anars-MacBook-Air.local>
2024-03-11 14:20:23 +0800

    starting

---
+++ pages/Header/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="px-28 pt-10">
+      <div className="navbar bg-[#23241f]">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/Introduction/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div className="p-28">
+      <div>
+        <img
+          src="./images/portfolio.jpg"
+          className="w-screen flex-wrap h-screen columns-1"
+        />
+      </div>
+
+      {/* <div className="diff aspect-[16/9]">
+        <div className="diff-item-1">
+          <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
+            Full Stack <span>Developer</span>
+          </div>
+        </div>
+        <div className="diff-item-2">
+          <div className="bg-base-200 text-9xl font-black grid place-content-center">
+            Anar
+          </div>
+        </div>
+        <div className="diff-resizer"></div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Introduction;
