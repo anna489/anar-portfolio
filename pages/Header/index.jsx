452711c6794553a3794b082c00408b18df187006
Anar Ganbaatar <LP230138@G301-CC0197.local>
2024-03-11 19:00:28 +0800

    header

--- pages/Header/index.jsx
+++ pages/Header/index.jsx
@@ -5,22 +5,31 @@ const Header = () => {
     <div className="px-28 pt-10">
       <div className="navbar bg-[#23241f]">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Portfolio</a>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Link</a>
+              <a>Home</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <details className="md:hidden">
+                <summary>Home</summary>
                 <ul className="p-2 bg-base-100 rounded-t-none">
                   <li>
-                    <a>Link 1</a>
+                    <a>Home</a>
                   </li>
                   <li>
-                    <a>Link 2</a>
+                    <a>About</a>
+                  </li>
+                  <li>
+                    <a>Projects</a>
                   </li>
                 </ul>
               </details>
